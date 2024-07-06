1bba5d446a6ab25d8ddfa1cccf20b2651ead2674
Abdisharif <sharifabdi735@gmail.com>
2024-07-06 15:11:05 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+function writeCards(names, event) {
+    let messages = [];
+    let i = 0;
+    while (i < names.length) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      i++;
+    }
+    return messages;
+  }
